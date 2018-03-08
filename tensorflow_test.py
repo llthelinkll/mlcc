d0435476f01950b29130c7576cf7292f2a476909
mos <s207.mos@gmail.com>
2018-03-08 21:35:28 +0700

    initial tensorflow_test

---
+++ tensorflow_test.py
@@ -0,0 +1,24 @@
+"""
+    SETUP
+"""
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+"""
+    Load data set
+""" 
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+print(repr(california_housing_dataframe))
